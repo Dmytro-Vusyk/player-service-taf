f7870bace822704ef73637d728288b7b93bca10f
dmytrovusyk <mail@mail.com>
2024-09-28 22:33:34 +0300

    add PlayerCreateRequestDTO

---
+++ models/src/main/java/com/companyname/models/playerserviceapi/PlayerCreateRequestDTO.java
@@ -0,0 +1,16 @@
+package com.companyname.models.playerserviceapi;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PlayerCreateRequestDTO {
+    private String age;
+    private String editor;
+    private String gender;
+    private String login;
+    private String password;
+    private String role;
+    private String screenName;
+}
