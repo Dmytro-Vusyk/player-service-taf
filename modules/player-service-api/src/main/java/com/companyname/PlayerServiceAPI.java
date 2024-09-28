8744302f1e28e40c2015ce7bbe69ab6bb01af485
dmytrovusyk <mail@mail.com>
2024-09-28 13:08:01 +0300

    add API implementation

--- modules/player-service-api/src/main/java/com/companyname/PlayerServiceAPI.java
+++ modules/player-service-api/src/main/java/com/companyname/PlayerServiceAPI.java
@@ -1,4 +1,30 @@
 package com.companyname;
 
+import com.companyname.endpoints.PlayerControllerEndpoint;
+
 public class PlayerServiceAPI {
+
+    private static PlayerServiceAPI playerServiceAPI;
+    //TODO: replace field initialization with config file
+    private static final String BASE_URL = "http://3.68.165.45/";
+    private PlayerControllerEndpoint playerControllerEndpoint;
+
+    private PlayerServiceAPI() {
+        initUnversionedEndpoints();
+    }
+
+    public static PlayerServiceAPI getAPI() {
+        if (PlayerServiceAPI.playerServiceAPI == null) {
+            PlayerServiceAPI.playerServiceAPI = new PlayerServiceAPI();
+        }
+        return PlayerServiceAPI.playerServiceAPI;
+    }
+
+    private void initUnversionedEndpoints() {
+        this.playerControllerEndpoint = new PlayerControllerEndpoint(BASE_URL);
+    }
+
+    public PlayerControllerEndpoint getPlayerControllerEndpoint() {
+        return this.playerControllerEndpoint;
+    }
 }
