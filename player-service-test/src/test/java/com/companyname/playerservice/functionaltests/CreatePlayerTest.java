bf6647d56cb26a5fbae71ee3caf1835878338f28
dmytrovusyk <mail@mail.com>
2024-09-30 22:20:07 +0300

    add allure and annotations

--- player-service-test/src/test/java/com/companyname/playerservice/functionaltests/CreatePlayerTest.java
+++ player-service-test/src/test/java/com/companyname/playerservice/functionaltests/CreatePlayerTest.java
@@ -3,6 +3,9 @@ package com.companyname.playerservice.functionaltests;
 import com.companyname.enums.PlayerEditors;
 import com.companyname.factories.PlayerCreateRequestDTOFactory;
 import com.companyname.models.playerserviceapi.PlayerCreateResponseDTO;
+import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
+import io.qameta.allure.testng.Tags;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.DataProvider;
@@ -10,13 +13,15 @@ import org.testng.annotations.Test;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
+@Feature("Create Player")
+@Story("Admin or Supervisor is able to create Player")
 public class CreatePlayerTest extends PlayerServiceTestSpec {
 
     private static Long createdPlayerId;
 
     @DataProvider(name = "roleProvider")
     public Object[][] roleProvider() {
-        return new Object[][] {
+        return new Object[][]{
                 {PlayerEditors.SUPERVISOR.getValue()},
                 {PlayerEditors.ADMIN.getValue()}
         };
@@ -29,6 +34,11 @@ public class CreatePlayerTest extends PlayerServiceTestSpec {
      * 2. Assert that status code is 200
      * 3. Assert that response schema is as expected
      */
+    @Description("As admin or supervisor I want to be able to create a player")
+    @Severity(SeverityLevel.CRITICAL)
+    @Issue("PS-1234")
+    @TmsLink("PS-321")
+    @Tags(@Tag("smoke"))
     @Test(dataProvider = "roleProvider")
     public void verifyThatPlayerCanBeCreated(String role) {
 
@@ -44,9 +54,10 @@ public class CreatePlayerTest extends PlayerServiceTestSpec {
                 .body(matchesJsonSchemaInClasspath("playerservice/schemas/create-player-response.json"));
     }
 
+    @Step("Cleanup: delete user with id: {createdPlayerId}")
     @AfterTest
-    void cleanup(){
-        this.playerControllerEndpoint.deletePlayer(PlayerEditors.SUPERVISOR.getValue(),createdPlayerId)
+    void cleanup() {
+        this.playerControllerEndpoint.deletePlayer(PlayerEditors.SUPERVISOR.getValue(), createdPlayerId)
                 .statusCode(HttpStatus.SC_NO_CONTENT);
     }
 }
--- player-service-test/src/test/java/com/companyname/playerservice/functionaltests/PlayerServiceTestSpec.java
+++ player-service-test/src/test/java/com/companyname/playerservice/functionaltests/PlayerServiceTestSpec.java
@@ -3,8 +3,10 @@ package com.companyname.playerservice.functionaltests;
 import com.companyname.BaseTestSpec;
 import com.companyname.PlayerServiceAPI;
 import com.companyname.endpoints.PlayerControllerEndpoint;
+import io.qameta.allure.Epic;
 import org.testng.annotations.BeforeClass;
 
+@Epic("Player service implementation")
 public class PlayerServiceTestSpec extends BaseTestSpec {
 
     protected PlayerControllerEndpoint playerControllerEndpoint;
