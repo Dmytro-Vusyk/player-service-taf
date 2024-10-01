38f9758ce7c8fdbcf7d134de68961aa8c2da2cd5
dmytrovusyk <mail@mail.com>
2024-10-01 13:25:42 +0300

    add negative test

---
+++ player-service-test/src/test/java/com/companyname/playerservice/functionaltests/CreatePlayerNegativeTest.java
@@ -0,0 +1,28 @@
+package com.companyname.playerservice.functionaltests;
+
+import com.companyname.TestGroups;
+import com.companyname.factories.PlayerCreateRequestDTOFactory;
+import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
+import io.qameta.allure.testng.Tags;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+
+@Feature("Create Player")
+@Story("Admin or Supervisor is able to create Player")
+public class CreatePlayerNegativeTest extends PlayerServiceTestSpec {
+
+    @Description("User with not specified role is not able to CREATE a Player")
+    @Severity(SeverityLevel.NORMAL)
+    @TmsLink("PS-323") //Test case id in TMS
+    @Tags(@Tag(TestGroups.NEGATIVE))
+    @Test(groups = {TestGroups.NEGATIVE, TestGroups.BVT})
+    public void verifyThat() {
+        var expectedPlayer = PlayerCreateRequestDTOFactory.createDefaultPlayer();
+        var role = "GOD";
+        this.playerControllerEndpoint.createPlayer(
+                role,
+                expectedPlayer,
+                HttpStatus.SC_FORBIDDEN);
+    }
+}
