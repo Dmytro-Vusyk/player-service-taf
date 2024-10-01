1b2101b5aebe6867ed36a8fa45d2d6fbdded1a45
dmytrovusyk <mail@mail.com>
2024-10-01 13:12:05 +0300

    update TestGroups

--- player-service-test/src/main/java/com/companyname/TestGroups.java
+++ player-service-test/src/main/java/com/companyname/TestGroups.java
@@ -4,4 +4,5 @@ public class TestGroups {
     public static final String SMOKE = "SMOKE";
     public static final String REG= "REGRESSION";
     public static final String BVT = "BVT";
+    public static final String NEGATIVE = "NEGATIVE";
 }
