0d348a85c78730e5f87e84100b02b27d44cff035
dmytrovusyk <mail@mail.com>
2024-10-03 01:56:37 +0300

    add get url from props

--- modules/player-service-api/src/main/java/com/companyname/PlayerServiceAPI.java
+++ modules/player-service-api/src/main/java/com/companyname/PlayerServiceAPI.java
@@ -1,6 +1,7 @@
 package com.companyname;
 
 
+import com.companyname.config.PropertiesHandler;
 import com.companyname.endpoints.PlayerControllerEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +10,13 @@ public class PlayerServiceAPI {
 
     private static final Logger logger = LoggerFactory.getLogger(PlayerServiceAPI.class);
     private static PlayerServiceAPI playerServiceAPI;
-    //TODO: replace field initialization with config file
-    private static final String BASE_URL = "http://3.68.165.45/";
+    private final String baseUrl;
     private final static ThreadLocal<PlayerControllerEndpoint> playerControllerEndpoint = new ThreadLocal<>();
 
     private PlayerServiceAPI() {
         logger.debug("Constructing Player Service API");
+        // this config should be moved to BaseAPI class(class should be created on commercial framework.)
+        this.baseUrl = PropertiesHandler.getInstance().getEnvProperties().getProperty("player.service.host");
         initUnversionedEndpoints();
     }
 
@@ -26,10 +28,10 @@ public class PlayerServiceAPI {
     }
 
     private void initUnversionedEndpoints() {
-        this.playerControllerEndpoint.set(new PlayerControllerEndpoint(BASE_URL));
+        playerControllerEndpoint.set(new PlayerControllerEndpoint(baseUrl));
     }
 
     public PlayerControllerEndpoint getPlayerControllerEndpoint() {
-        return this.playerControllerEndpoint.get();
+        return playerControllerEndpoint.get();
     }
 }
