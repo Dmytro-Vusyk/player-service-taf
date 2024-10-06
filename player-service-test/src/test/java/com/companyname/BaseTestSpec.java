31d402b5441ab30479bdc208cb224750ffac5335
Dmytro-Vusyk <Dmytro-Vusyk@users.noreply.github.com>
2024-10-06 17:16:59 +0000

    deploy: 230969828c880506b5c3b5a70bf5ee99b73a5e12

--- player-service-test/src/test/java/com/companyname/BaseTestSpec.java
+++
@@ -1,9 +0,0 @@
-package com.companyname;
-
-import com.companyname.testutils.listeners.LoggingTestListener;
-import com.companyname.testutils.listeners.ThreadCountListener;
-import org.testng.annotations.Listeners;
-
-//@Listeners({LoggingTestListener.class, ThreadCountListener.class})
-public class BaseTestSpec {
-}
