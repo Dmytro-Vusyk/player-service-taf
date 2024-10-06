1dc013182965bf39604d77d3916113137a2a9d30
Dmytro-Vusyk <Dmytro-Vusyk@users.noreply.github.com>
2024-10-06 16:22:18 +0000

    deploy: 230969828c880506b5c3b5a70bf5ee99b73a5e12

--- player-service-core/src/main/java/com/companyname/testutils/TestGroups.java
+++
@@ -1,9 +0,0 @@
-package com.companyname.testutils;
-
-public class TestGroups {
-    public static final String SMOKE = "SMOKE";
-    public static final String REG= "REGRESSION";
-    public static final String BVT = "BVT";
-    public static final String NEGATIVE = "NEGATIVE";
-    public static final String BUG = "BUG";
-}
