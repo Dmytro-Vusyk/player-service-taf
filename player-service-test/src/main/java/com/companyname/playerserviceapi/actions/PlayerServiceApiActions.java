7cfd46fe36ca2960a5fbd502892b987bb8adc832
dmytrovusyk <mail@mail.com>
2024-10-09 12:32:22 +0300

    add overloaded methods

--- player-service-test/src/main/java/com/companyname/playerserviceapi/actions/PlayerServiceApiActions.java
+++ player-service-test/src/main/java/com/companyname/playerserviceapi/actions/PlayerServiceApiActions.java
@@ -2,10 +2,10 @@ package com.companyname.playerserviceapi.actions;
 
 import com.companyname.PlayerServiceAPI;
 import com.companyname.endpoints.PlayerControllerEndpoint;
-import com.companyname.models.playerserviceapi.PlayerCreateRequestDTO;
-import com.companyname.models.playerserviceapi.PlayerUpdateRequestDTO;
+import com.companyname.models.playerserviceapi.*;
 import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
 
 public class PlayerServiceApiActions {
 
@@ -21,9 +21,16 @@ public class PlayerServiceApiActions {
         return this.playerControllerEndpoint.createPlayer(editor, player);
     }
 
+    public PlayerCreateResponseDTO createPlayer(String editor, PlayerCreateRequestDTO player, int expectedStatusCode) {
+        return createPlayer(editor, player).statusCode(expectedStatusCode)
+                .extract()
+                .body()
+                .as(PlayerCreateResponseDTO.class);
+    }
+
     @Step("Update player")
     public ValidatableResponse updatePlayer(String editor, Long playerToUpdateId, PlayerUpdateRequestDTO playerPayload) {
-       return this.playerControllerEndpoint.updatePlayer(editor, playerToUpdateId, playerPayload);
+        return this.playerControllerEndpoint.updatePlayer(editor, playerToUpdateId, playerPayload);
     }
 
     @Step("Delete player")
@@ -36,8 +43,23 @@ public class PlayerServiceApiActions {
         return this.playerControllerEndpoint.getPlayerById(playerId);
     }
 
+    public PlayerGetByPlayerIdResponseDTO getPlayerById(Long playerId, int expectedStatusCode) {
+        return getPlayerById(playerId).statusCode(expectedStatusCode)
+                .extract()
+                .body()
+                .as(PlayerGetByPlayerIdResponseDTO.class);
+    }
+
     @Step("Get all players")
     public ValidatableResponse getAllPlayers() {
         return this.playerControllerEndpoint.getAllPlayers();
+    }
+
+    public PlayersDTO getAllPlayers(int expectedStatusCode) {
+        return getAllPlayers()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .body()
+                .as(PlayersDTO.class);
     }
 }
